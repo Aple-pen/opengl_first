12b0abde9352a70f8b33a36dfb51441ddb16695f
eunggi <rubie3374@naver.com>
2025-02-06 20:41:00 +0900

    first commit

---
+++ Dependency.cmake
@@ -0,0 +1,45 @@
+﻿#ExternalProject 관련 명령어 셋 추가
+include(ExternalProject)
+
+#Dependency 관련 변수 설정
+set(DEP_INSTALL_DIR ${PROJECT_BINARY_DIR}/install) #PROJECT_BINARY_DIR  : build 폴더
+set(DEP_INCLUDE_DIR ${DEP_INSTALL_DIR}/include)
+set(DEP_LIB_DIR ${DEP_INSTALL_DIR}/lib)
+
+# spdlog: fast logger library
+ExternalProject_Add(
+    dep_spdlog
+    GIT_REPOSITORY "https://github.com/gabime/spdlog.git"
+    GIT_TAG "v1.x"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ""
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+    TEST_COMMAND ""     
+)
+# GIT_SHALLOW : 1(true) 가장 최신의 commit 만을 받겠다.
+
+# 추가: /utf-8 플래그를 설정합니다
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /utf-8")
+# Dependency 리스트 및 라이브러리 파일 리스트 추가
+set(DEP_LIST ${DEP_LIST} dep_spdlog)
+set(DEP_LIBS ${DEP_LIBS} spdlog$<$<CONFIG:Debug>:d>) # config가 debug일때는 d를 붙이고 아닐땐 d를 붙이지 말아라.
+
+#GLFW 설치
+ExternalProject_Add(
+    dep_glfw
+    GIT_REPOSITORY "https://github.com/glfw/glfw.git"
+    GIT_TAG "3.3.3"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ""
+    TEST_COMMAND ""
+    CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+        -DGLFW_BUILD_EXAMPLES=OFF
+        -DGLFW_BUILD_TESTS=OFF
+        -DGLFW_BUILD_DOCS=OFF
+) 
+
+set(DEP_LIST ${DEP_LIST} dep_glfw)
+set(DEP_LIBS ${DEP_LIBS} glfw3)
